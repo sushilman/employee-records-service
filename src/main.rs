9e06fa35a4ab66b691ef6a869316044eedb5c348
Sushil <sushilman@gmail.com>
2022-05-29 17:15:13 +0200

    Setting up rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[launch]
+fn app() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Employee Records API"
 }
